e8666d23a056b77a02633d5c3ed1daf7d1a10edd
akambi <akambi@speedwapp.com>
2017-03-06 05:15:39 -0800

    fix check mcsb container is defined when destroy

--- jquery.mCustomScrollbar.js
+++ jquery.mCustomScrollbar.js
@@ -1104,6 +1104,7 @@ and dependencies (minified).
 				/* clear and delete timeouts/objects */
 				clearTimeout($this[0]._focusTimeout); _delete($this[0],"_focusTimeout");
 				clearTimeout(d.sequential.step); _delete(d.sequential,"step");
+				if (!mCSB_container.length) { return; }
 				clearTimeout(mCSB_container[0].onCompleteTimeout); _delete(mCSB_container[0],"onCompleteTimeout");
 				d.bindEvents=false;
 			}
